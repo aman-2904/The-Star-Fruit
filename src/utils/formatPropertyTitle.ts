3b974f34a5793407d05e809ed6adaac83649e687
luxevillaz <luxevillaz@gmail.com>
2026-09-04 12:51:18 +0530

    Update formatPropertyTitle.ts ( replace LV to LZ )

--- src/utils/formatPropertyTitle.ts
+++ src/utils/formatPropertyTitle.ts
@@ -1,6 +1,6 @@
 export function formatPropertyTitle(title?: string | null, lsId?: string | null): string {
   if (!title) return '';
   if (!lsId) return title;
-  const displayId = lsId.replace(/^LS/i, 'LV');
+  const displayId = lsId.replace(/^LS/i, 'LZ');
   return `${displayId} | ${title}`;
 }
